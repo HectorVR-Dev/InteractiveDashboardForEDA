df05d1bc120e934d5505ea1d62e181219d30299e
Héctor Vásquez <70999302+HectorVR-Dev@users.noreply.github.com>
2024-03-01 00:52:21 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -690,6 +690,14 @@ class dashboard():
         # Para cada integrante, se presenta su nombre, rol, responsabilidades, afiliación universitaria y dirección de correo electrónico.
         st.title("Feedback y Contacto")
         st.header("Integrantes")
+        
+        st.subheader("Hector Daniel Vasquez Rivera")
+        st.write("**Rol**: Programador, Tester, Analista y Lider")
+        st.write("**Responsabilidades**:  Diseñar la interfaz de usuario, del dashboard, para garantizar una experiencia de usuario intuitiva y atractiva. Encargado de realizar análisis de datos y generar visualizaciones significativas.")
+        st.write("**Afiliación**: Estudiante en Ingeniería Mecatrónica y Estadística de la Universidad Nacional de Colombia sede de La Paz")
+        st.write(
+            "**Contacto** :email:: [hevasquezr@unal.edu.co](mailto:hevasquezr@unal.edu.co)")
+        
         st.subheader("Wilhelm David Buitrago Garcia")
         st.write("**Rol**: Programador, Analista, Colider")
         st.write("**Responsabilidades**:  Desarrollar la lógica del sistema de filtros y graficas, gestionar la integración de datos, realizar análisis de datos y generar visualizaciones significativas.")
@@ -703,13 +711,6 @@ class dashboard():
         st.write("**Afiliación**: Estudiante en Ingeniería Mecatrónica de la Universidad Nacional de Colombia sede de La Paz")
         st.write(
             "**Contacto** :email:: [seguzmanc@unal.edu.co](mailto:seguzmanc@unal.edu.co)")
-
-        st.subheader("Hector Daniel Vasquez Rivera")
-        st.write("**Rol**: Programador, Tester, Analista y Lider")
-        st.write("**Responsabilidades**:  Diseñar la interfaz de usuario, del dashboard, para garantizar una experiencia de usuario intuitiva y atractiva. Encargado de realizar análisis de datos y generar visualizaciones significativas.")
-        st.write("**Afiliación**: Estudiante en Ingeniería Mecatrónica y Estadística de la Universidad Nacional de Colombia sede de La Paz")
-        st.write(
-            "**Contacto** :email:: [hevasquezr@unal.edu.co](mailto:hevasquezr@unal.edu.co)")
 
     def desc_var(self, var):
         # recibe como entrada el nombre de una variable var y devuelve una descripción correspondiente a esa variable.
