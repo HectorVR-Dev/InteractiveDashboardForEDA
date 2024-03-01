66fb75175c902a453c911b52eeacbc252bbdd42a
HectorVR-Dev <hectordaniel1112@gmail.com>
2024-02-29 22:31:18 -0500

    Add conclusiones page

--- src/app.py
+++ src/app.py
@@ -20,7 +20,7 @@ class dashboard():
         self.plt = plt
         st.sidebar.title("Navegación")
         self.page = st.sidebar.radio("", ["Inicio", "EDA and Visualización", "Filtros Interactivos",
-                                     "Conclusiones y Recomendaciones", "Recursos Adicionales", "Feedback y Contacto"])
+                                     "Conclusiones", "Recursos Adicionales", "Feedback y Contacto"])
         self.vars = self.df.columns.to_list()
         self.vars.insert(0, "")
         st.sidebar.image(img, width=200)
@@ -538,9 +538,29 @@ class dashboard():
             fuction(label, column, options)
 
     def show_conclusions(self):
-        st.title("Conclusiones y Recomendaciones")
-        st.write(
-            "Aquí encontrarás las conclusiones clave y las recomendaciones basadas en el análisis de datos.")
+        st.title("Hallazgos del Análisis de Datos")
+        st.write("""
+        1. **Distribución de Género:**
+           - El 55% de los estudiantes son hombres y el 45% son mujeres. Esta diferencia es más evidente en el programa de Mecatrónica, donde solo el 8.3% son mujeres.
+
+        2. **Situación Socioeconómica:**
+           - El 72% de los estudiantes tienen el Puntaje de Matrícula Básica (PBM) entre 0 y 10, lo que indica que no pagan matrícula y una gran parte se puede considerar vulnerable. Además, casi el 90% de los estudiantes son de estratos 1 y 2.
+
+        3. **Rendimiento Académico:**
+           - A medida que se avanza en la carrera, el promedio académico tiende a estabilizarse en alrededor de 4.2.
+
+        4. **Preferencia de Carreras:**
+           - Las ingenierías son los programas más demandados, con los puntajes de admisión más altos y la menor tasa de deserción, indicando una mayor cantidad de estudiantes activos.
+
+        5. **Residencia Estudiantil:**
+           - La mayoría de los estudiantes residen en el municipio de Valledupar.
+
+        6. **Correlación entre Tipo de Colegio y Puntaje de Admisión:**
+           - Se aprecia una posible correlación entre el tipo de colegio y el puntaje de admisión, con estudiantes de colegios privados tendiendo a tener puntajes más altos que los de colegios públicos y nocturnos. Esto sugiere diferencias en la calidad de la educación entre diferentes tipos de colegios.
+        
+        7. **Origen Regional:**
+           - Más del 90% de los estudiantes son nacidos en la región Caribe, donde se encuentra ubicada la universidad.
+        """)
 
     def show_resources(self):
         st.title("Recursos Adicionales")
