7c846e6191a6bd5b7bb529da8d18e097d12930f7
WilhelmBuitrago <122063634+WilhelmBuitrago@users.noreply.github.com>
2024-08-21 23:19:17 -0500

    app v2.0 (#14)
    * modify function for dataframe querys (shape)
    
    * app v2.0
    
    * .gitignored update

--- pruebas.py
+++ pruebas.py
@@ -2,7 +2,20 @@ import oracledb
 import streamlit as st
 from src.utils import GenerateQuerys, convert_oracle_to_df
 
-params = {
-    "FROM": "ESTUDIANTES_DIRTY"
+query_singles = {
+    "SELECT": "EC.COD_PLAN, CP.PLAN",
+    "FROM": {
+        "table": "ESTUDIANTES_CLEAR",
+        "as": "EC"
+    },
+    "JOIN": {
+        "table": "COD_PLAN",
+        "as": "CP",
+        "on": "COD_PLAN"
+    },
+    "GROUP BY": "EC.COD_PLAN"
 }
-print(convert_oracle_to_df(params).head(2))
+
+query = GenerateQuerys(query_singles)
+print(query)
+print(convert_oracle_to_df(query_singles))
